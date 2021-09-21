644e98f03eba739dc276cae96f9ef87a39aefedf
p22melsheikh <p22melsheikh@fairfieldprep.org>
2021-09-21 10:18:11 -0400

    labs

---
+++ lab_3-1.py
@@ -0,0 +1,13 @@
+# author__= ME 9/20/2021
+
+a = 2
+print(type(a))
+
+a = float(a)
+print(type(a))
+
+a = str(a)
+print(type(a))
+
+a = bool(a)
+print(a==2)
---
+++ lab_4-1.py
@@ -0,0 +1,11 @@
+# author__= ME 9/20/2021
+
+a = 1
+b =2
+c = 3
+x = 2
+y = 5
+
+equation= (3+4*x)/5-(10*(y-5)*(a+b+c)**2)/x+9*(4/x**2+(9+x)/y)
+
+print(equation)
